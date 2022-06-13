55ed466695ad935b1b26273fcc5f5165902bdb3f
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-06-13 05:18:59 -0300

    add tooltip

--- src/pages/components/Info.tsx
+++ src/pages/components/Info.tsx
@@ -1,4 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Icon, Text, Tooltip } from "@chakra-ui/react";
+import { RiInformationLine } from "react-icons/ri";
+
 import React from "react";
 
 interface InfoProps {
@@ -8,17 +10,40 @@ interface InfoProps {
 
 export function Info({ value, description }: InfoProps) {
   return (
-    <Text
-      display="flex"
+    <Flex
       flexDir="column"
       fontWeight={[400, 400, 600]}
       fontSize={["18", "18", "24"]}
       align={["start", "start", "center"]}
     >
-      <Text as="span" color="yellow.400" fontSize={["24", "24", "5xl"]} fontWeight={600}>
+      <Text
+        as="span"
+        color="yellow.400"
+        fontSize={["24", "24", "5xl"]}
+        fontWeight={600}
+      >
         {value}
       </Text>
-      {description}
-    </Text>
+      <Text>
+        {description}
+        <Tooltip
+          label="100 cidades mais visitadas do mundo"
+          closeDelay={1000}
+          hasArrow
+          placement="top"
+        >
+          <span>
+            <Icon
+              cursor="pointer"
+              as={RiInformationLine}
+              ml="1"
+              color="gray.400"
+              w={["10px", "16px"]}
+              h={["10px", "16px"]}
+            />
+          </span>
+        </Tooltip>
+      </Text>
+    </Flex>
   );
 }
